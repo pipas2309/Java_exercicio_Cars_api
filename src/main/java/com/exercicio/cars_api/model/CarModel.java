3ece4a63fcb3bac0dbccb803801f1ca6bfe38a27
Lucas Palharini <lucas230991@hotmail.com>
2023-01-19 10:39:55 -0300

    :sparkles: feat: update carModel

--- src/main/java/com/exercicio/cars_api/model/CarModel.java
+++ src/main/java/com/exercicio/cars_api/model/CarModel.java
@@ -1,5 +1,10 @@
 package com.exercicio.cars_api.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +12,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 public class CarModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
     private String modelo;
+    
+    @Column(length = 200, nullable = false)
     private String fabricante;
+    
+    @Column(length = 30, nullable = false)
     private String dataFabricacao;
+    
+    @Column(length = 10, nullable = false)
     private double valor;
+    
+    @Column(length = 4, nullable = false)
     private int anoModelo;
 }
