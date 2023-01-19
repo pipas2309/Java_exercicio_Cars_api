d5fd4537c7b893e99a3d7a231efa1fb987495e76
Lucas Palharini <lucas230991@hotmail.com>
2023-01-18 22:28:04 -0300

    :bug: fix: change type to CarDTO

--- src/main/java/com/exercicio/cars_api/controller/BuilderController.java
+++ src/main/java/com/exercicio/cars_api/controller/BuilderController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.exercicio.cars_api.dto.CarDTO;
+
 @RestController
 @RequestMapping("/api")
 public class BuilderController {
     
     @PostMapping("/post")
-    public String create(@RequestBody String req) {
+    public CarDTO create(@RequestBody CarDTO req) {
         System.out.println(req);
         return req;
     }
