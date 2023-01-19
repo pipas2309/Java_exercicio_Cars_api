c5fe891a41df97b46dcc48065823c52a1dba22a8
Lucas Palharini <lucas230991@hotmail.com>
2023-01-19 11:18:02 -0300

    :construction: feat: update POST endPoint

--- src/main/java/com/exercicio/cars_api/controller/BuilderController.java
+++ src/main/java/com/exercicio/cars_api/controller/BuilderController.java
@@ -1,6 +1,6 @@
 package com.exercicio.cars_api.controller;
 
-import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.exercicio.cars_api.dto.CarDTO;
 import com.exercicio.cars_api.model.CarModel;
+import com.exercicio.cars_api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/api")
 
 public class BuilderController {
 
-    private CarModel car = new CarModel();
+    @Autowired
+    private CarRepository repository;
 
     @GetMapping(value = {"", "/", "/post"})
     public ResponseEntity<String> justAMessage() {
@@ -24,23 +26,7 @@ public class BuilderController {
     }
     
     @PostMapping("/post")
-    public CarModel create(@RequestBody CarDTO req)  {
-        BeanUtils.copyProperties(req, car);
-
-        if(req.modelo() == null || req.modelo().equals("")) {
-            car.setModelo("Carro");
-        }
-        if(req.fabricante() == null ||req.fabricante().equals("")) {
-            car.setFabricante("Criado através de geração espontânea");
-        }
-        if(req.dataFabricacao() == null || req.dataFabricacao().equals("")) {
-            car.setDataFabricacao("Sempre/Exis/tiu");
-        }
-        if(req.anoModelo() == 0) {
-            car.setAnoModelo(666);
-        }
-        System.out.println(car);
-
-        return car;
+    public void create(@RequestBody CarDTO req)  {
+        repository.save(new CarModel(req));
     }
 }
