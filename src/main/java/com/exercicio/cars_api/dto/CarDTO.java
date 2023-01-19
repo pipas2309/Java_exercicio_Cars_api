bc19eed9cd5a8b709beaf2174b3cd35497deb753
Lucas Palharini <lucas230991@hotmail.com>
2023-01-18 23:35:57 -0300

    :bug: fix: remove uppercase

--- src/main/java/com/exercicio/cars_api/dto/CarDTO.java
+++ src/main/java/com/exercicio/cars_api/dto/CarDTO.java
@@ -1,5 +1,5 @@
 package com.exercicio.cars_api.dto;
 
-public record CarDTO(String modelo, String Fabricante, String dataFabricacao, double valor, int anoModelo) {
+public record CarDTO(String modelo, String fabricante, String dataFabricacao, double valor, int anoModelo) {
     
 }
