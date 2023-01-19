0d8be581f4971d119c424e03d559c99886bc0c5e
Lucas Palharini <lucas230991@hotmail.com>
2023-01-18 21:52:39 -0300

    :tada: Initial commit

---
+++ src/main/java/com/exercicio/cars_api/CarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.cars_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsApiApplication.class, args);
+	}
+
+}
