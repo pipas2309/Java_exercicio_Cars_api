9c011f9d0b5aa618a8625712c809b8beba145aa1
Lucas Palharini <lucas230991@hotmail.com>
2023-01-20 02:51:24 -0300

    :vertical_traffic_light: feat: add validation to carDTO

--- src/main/java/com/exercicio/cars_api/dto/CarDTO.java
+++ src/main/java/com/exercicio/cars_api/dto/CarDTO.java
@@ -1,5 +1,23 @@
 package com.exercicio.cars_api.dto;
 
-public record CarDTO(String modelo, String fabricante, String dataFabricacao, double valor, int anoModelo) {
-    
+import java.time.Year;
+import java.util.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
+public record CarDTO(
+
+        @NotBlank String modelo,
+
+        @NotBlank String fabricante,
+
+        @NotNull @Past Date dataFabricacao,
+
+        @NotNull @Positive double valor,
+
+        @NotNull @PastOrPresent Year anoModelo) {
 }
