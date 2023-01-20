3b3592ddb6f9c0a70d3d25a10edb1240ed1088d4
Lucas Palharini <lucas230991@hotmail.com>
2023-01-20 02:58:38 -0300

    :sparkles: feat: create DELETE endPoint

--- src/main/java/com/exercicio/cars_api/controller/BuilderController.java
+++ src/main/java/com/exercicio/cars_api/controller/BuilderController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,10 @@ public class BuilderController {
     @PostMapping("/post")
     public void create(@RequestBody @Valid CarDTO req)  {
         repository.save(new CarModel(req));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
